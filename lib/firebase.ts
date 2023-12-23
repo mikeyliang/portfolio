43d3e4e07b68d83fc5de5f0d16d4f0ba81b9a58d
mikeyliang <liangmichael27822@gmail.com>
2023-12-23 01:18:51 -0700

    firebase config reset

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,13 +8,13 @@ import {getStorage, FirebaseStorage} from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBwiE6tMX3E4jBxtuG6WHfIiTpKQzsYEjo",
-  authDomain: "portfolio-90183.firebaseapp.com",
-  projectId: "portfolio-90183",
-  storageBucket: "portfolio-90183.appspot.com",
-  messagingSenderId: "420711109730",
-  appId: "1:420711109730:web:6e8c1298ad3b5a9d3b6d3b",
-  measurementId: "G-VVWJ0L5JES",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 let app;
